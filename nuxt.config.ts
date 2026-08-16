f7961dca4718936c7d14a8af9a39f759eb9af99d
yasirmaulana <yasir.maulana@gmail.com>
2026-08-16 17:14:40 +0700

    feat: tambah @vercel/speed-insights ke nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: process.env.NODE_ENV !== 'production' },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@vercel/speed-insights/nuxt'],
   css: ['~/assets/css/main.css'],
   app: {
     head: {
